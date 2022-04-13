d2ec661af677ba1d52cbf86d655cfcc9011d5ee2
George Seed <50909308+georgeseed@users.noreply.github.com>
2022-04-13 12:38:09 +0100

    Update LandG_native2.js

--- LandG_native2.js
+++ LandG_native2.js
@@ -50,6 +50,6 @@ var clickFunction = function() {
 setTimeout(function(){
     target.addEventListener('click', clickFunction, true);
 target.addEventListener('touchstart', clickFunction, true);
-},1100);
+},1000);
 
 }, 1000);
