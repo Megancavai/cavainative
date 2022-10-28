61f59df382ca8a0c312897a23339fff68cd04989
Megan <92166789+Megancavai@users.noreply.github.com>
2022-10-28 16:07:30 +0100

    Update AYL_AMEX_new.js

--- AYL_AMEX_new.js
+++ AYL_AMEX_new.js
@@ -1,7 +1,4 @@
-setTimeout(function(){ 
-        
-    // Converse ID 463-2809-3741-22183 generated 2022-10-25T12:31:26.920Z
-    (function() {
+(function() {
         function async_load(target){
             var s = target.document.createElement('script');
             s.type = 'text/javascript';
@@ -22,35 +19,47 @@ setTimeout(function(){
         }
     })();
     
-
-var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
-var clickElement = document.createElement("div");
-clickElement.style.position = "absolute";
-clickElement.style.left = "0px";
-clickElement.style.top = "0px";
-clickElement.style.width = "100%";
-clickElement.style.height = "100%";
-if(target != null){
-    target.appendChild(clickElement);    
-}else{
-    target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
-}
-
 var clickFunction = function() {
-    
-    /*cavai code for clicking*/
-    const openBubble = new MouseEvent('click');
-    document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
 
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
+        /*cavai code for clicking*/
+        const openBubble = new MouseEvent('click');
+        document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+
+function winLoad(callback) {
+  if (document.readyState === 'complete') {
+    callback();
+  } else {
+    window.addEventListener("load", callback);
+  }
 }
 
+winLoad(function() {
+    console.log('page is fully loaded');
+        try {
+          setTimeout(function(){
+          var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+          var clickElement = document.createElement("div");
+          clickElement.style.position = "absolute";
+          clickElement.style.left = "0px";
+          clickElement.style.top = "0px";
+          clickElement.style.width = "100%";
+          clickElement.style.height = "100%";
+          if(target != null){
+              target.appendChild(clickElement);
+              target.addEventListener('click', clickFunction, true);
+              target.addEventListener('touchstart', clickFunction, true);
+          } else{
+              target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+              target.addEventListener('click', clickFunction, true);
+              target.addEventListener('touchstart', clickFunction, true);
+          }},1000);
 
-setTimeout(function(){
-    target.addEventListener('click', clickFunction, true);
-target.addEventListener('touchstart', clickFunction, true);
-},2000);
-
-}, 1000);
+          } catch (e) {
+            console.log(e)
+          }
+        });
