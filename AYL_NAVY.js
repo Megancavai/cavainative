cb32849d780497de69a70dd265ee6dd8029e2ff5
Megan <92166789+Megancavai@users.noreply.github.com>
2023-01-16 17:41:59 +0000

    Create AYL_NAVY.js

---
+++ AYL_NAVY.js
@@ -0,0 +1,73 @@
+(function() {
+        function async_load(target){
+            var s = target.document.createElement('script');
+            s.type = 'text/javascript';
+            s.id = 'cavai-script';
+            var theUrl = 'https://cloud.cavai.com/js/converses/widgets/converse-widget.1673890690873-1671708407169-1670922187183-1670836671509-d3679b1e7f96f6b763e823c10e551526.min.js?bust=' + Date.now();
+            s.src = theUrl;
+            s.async = true;
+            
+            s.dataset.clickMacro = 'MACRO_PLACEHOLDER';
+            s.dataset.bubbleAppearance = 'custom'
+
+            target.document.body.appendChild(s);
+        }
+        try {
+          async_load(window.top)
+        } catch (error) {
+          async_load(window)
+        }
+    })();
+    
+document.querySelector('# ').addEventListener('click', function() {
+	const openBubble = new MouseEvent('click');
+	document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+});
+</script>
+
+ var clickFunction = function() {
+
+        /*cavai code for clicking*/
+        const openBubble = new MouseEvent('click');
+        document.querySelector('.adbotic-chatbot-bubble-mob').dispatchEvent(openBubble);
+
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+
+function winLoad(callback) {
+  if (document.readyState === 'complete') {
+    callback();
+  } else {
+    window.addEventListener("load", callback);
+  }
+}
+
+winLoad(function() {
+    console.log('page is fully loaded');
+        try {
+          setTimeout(function(){
+          var target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+          var clickElement = document.createElement("div");
+          clickElement.style.position = "absolute";
+          clickElement.style.left = "0px";
+          clickElement.style.top = "0px";
+          clickElement.style.width = "100%";
+          clickElement.style.height = "100%";
+          if(target != null){
+              target.appendChild(clickElement);
+              target.addEventListener('click', clickFunction, true);
+              target.addEventListener('touchstart', clickFunction, true);
+          } else{
+              target = top.document.querySelectorAll('.ayl_v_ckr_b')[0];
+              target.addEventListener('click', clickFunction, true);
+              target.addEventListener('touchstart', clickFunction, true);
+          }},1000);
+
+          } catch (e) {
+            console.log(e)
+          }
+        });
+
+
